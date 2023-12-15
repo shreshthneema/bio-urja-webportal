0bcf79d812ac36bb3744cd5dcadaea92aa655116
huzefa <huzefa.zeenwal@gmail.com>
2023-12-15 15:06:07 +0530

    minor changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:webportal_biourja_flutter/main.dart';
+import 'package:webportal_biourja_flutter/core/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MainApp(
+      initialTheme: ThemeData(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
